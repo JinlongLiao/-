114517ea3614f3ea68611c2d90e4d142f7613b8b
廖金龙 <jinlongliao@foxmial.com>
2018-05-17 22:08:36 +0800

    基本 PHP 简单修改

---
+++ php-eureka/src/index.php
@@ -0,0 +1,10 @@
+<h1>It Work</h1>
+<a href="./doRegister.php" target="nnn" > 去注册一下</a>
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: liaojl
+ * Date: 2018/5/17
+ * Time: 20:29
+ */
+header('Content-Type:text/html;charset=UTF-8');
