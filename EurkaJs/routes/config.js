82e8d48eeed7c53dacf1cbbcd95e13e97332b750
廖金龙 <jinlongliao@foxmial.com>
2018-05-18 12:24:58 +0800

    NodeJs 模块的 上传注释打开

--- EurkaJs/routes/config.js
+++ EurkaJs/routes/config.js
@@ -45,16 +45,16 @@ router.post('/', function (req, res) {
     // 定义了一个post变量，用于暂存请求体的信息
     var post = '';
 
-    // 通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
-    req.on('data', function (chunk) {
-        post += chunk;
-    });
-
-    // 在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
-    req.on('end', function () {
-        post = querystring.parse(post);
-        res.end(util.inspect(post));
-    });
+    // // 通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
+    // req.on('data', function (chunk) {
+    //     post += chunk;
+    // });
+    //
+    // // 在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
+    // req.on('end', function () {
+    //     post = querystring.parse(post);
+    //     // res.end(util.inspect(post));
+    // });
     upload(req, res, function (err) {
         if (err) {
             throw err;
@@ -72,8 +72,8 @@ var Storage = multer.diskStorage({
     },
     filename: function (req, file, callback) {
         var fileName = file.fieldname + "_" + Date.now() + "_" + file.originalname;
-        // callback(null, fileName);
-        // doDataDisk(req, fileName);
+        callback(null, fileName);
+        doDataDisk(req, fileName);
     }
 });
 
